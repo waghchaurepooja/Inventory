c7894c0f8b3d3f78a8a3a6d00e0d8057ce0b7359
waghchaurepooja <97495331+waghchaurepooja@users.noreply.github.com>
2022-05-02 22:09:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Inventory } from "./components/Inventory";
+
+function App() {
+  return (
+    <div className="App">
+      helloworld
+      <Inventory />
+    </div>
+  );
+}
+
+export default App;
